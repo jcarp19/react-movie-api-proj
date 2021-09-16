baa9cf0ad817e5b9582e4d3041b94d8bfd8d7fce
jcarp19 <jim@carpenteronline.us>
2021-09-16 18:51:07 -0400

    added env.local

--- src/services/getMovies.tsx
+++ src/services/getMovies.tsx
@@ -3,11 +3,10 @@ import axios from "axios";
 import Movie from "../models/Movie";
 
 export default function getMovies(): Promise<Movie[]> {
-    const api_Key = "322003cc5ee7f07a72d0f0de66ac55a3"    
+     const apiKey = process.env.REACT_APP_MOVIES_APP_KEY 
     return axios
-        .get("https://api.themoviedb.org/3/movie/top_rated?api_key=322003cc5ee7f07a72d0f0de66ac55a3&language=en-US&page=1")
+        .get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
         .then(response => {
             return response.data.results;
-        })
-        
+        })    
   }
