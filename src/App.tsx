4bac87f9918205b8e503ed470002da952e09f108
jcarp19 <jim@carpenteronline.us>
2021-09-19 09:04:43 -0400

    simple css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,43 @@
-
+import React from "react";
 import './App.css';
 import MovieList from './components/MovieList';
 import SearchForm from "./components/SearchForm"
 import Favorites from './components/Favorites';
+import Logo from "./movieNite.jpg";
 import {Switch, Route, BrowserRouter as Router, NavLink} from "react-router-dom";
-// const [movies, setMovies] = useState<Movie[]>([]);
+
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <header>
-          <nav>
-            <ul>
-              <li><NavLink exact to="/">Home</NavLink></li>
-              <li><NavLink exact to="/search">Search</NavLink></li>
-              <li><NavLink exact to="/favorites">Favorites</NavLink></li>
-            </ul>
-          </nav>
-        </header>
-        <Switch>
-          <Route path="/favorites">
-            <Favorites />
-          </Route>
-          <Route path="/search">
-            <SearchForm />
-          </Route>
-          <Route path="/">
-            <h1>Home Page</h1>
-            <MovieList />
-          </Route>
-        </Switch>
-      </Router>
+    <div className="wrapper">
+      <div className="App">
+        <Router>
+          <header>
+            <div className="logo-box">
+              <img src={Logo} alt="logo" className="logo" />
+            </div>
+            <nav>
+              <ul>
+                <li><NavLink exact to="/">Home</NavLink></li>
+                <li><NavLink exact to="/search">Search</NavLink></li>
+                <li><NavLink exact to="/favorites">Favorites</NavLink></li>
+              </ul>
+            </nav>
+          </header>
+          <Switch>
+            <Route path="/favorites">
+              <Favorites />
+            </Route>
+            <Route path="/search">
+              <SearchForm />
+            </Route>
+            <Route path="/">
+              <h1>Home Page</h1>
+              <MovieList />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from "react";
 
 import Movie from "../models/Movie";
 import GetMovies from "../services/GetMovies";
+import "./MovieList.css";
 import SearchForm from "./SearchForm";
 import {useParams} from "react-router";
 
@@ -20,8 +21,8 @@ export default function MovieList() {
         <div>
             {movie?.slice(0,10).map((singleMovie, index) => {
                 return (
-                    <div key={index}>
-                        <p>{singleMovie?.title}</p>
+                    <div key={index} className="movieBox">
+                        <h2>{singleMovie?.title}</h2>
                         <p>{singleMovie?.vote_average}</p>
                     </div>
                 )
